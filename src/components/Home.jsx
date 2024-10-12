5151689edbb9a9ebdc685f6fdc74aea1f6b17ed2
neilrathi17 <neilrathi17@gmail.com>
2024-10-12 20:00:15 +0530

    initial comit

---
+++ src/components/Home.jsx
@@ -0,0 +1,105 @@
+
+import React, { useState, useEffect } from 'react';
+import {Fade, Slide} from 'react-awesome-reveal';
+import ReactMarkdown from 'react-markdown';
+import { Container, Row } from 'react-bootstrap';
+// import PropTypes from 'prop-types';
+import Social from './Social';
+import FallbackSpinner from './FallbackSpinner';
+import endpoints from '../constants/endpoints';
+// import Header from './Header';
+
+const styles = {
+  portfolioContainer: {
+    display: 'flex',
+    height: '100vh', // Make sure it takes the full height of the viewport
+  },
+  homeContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: '.5px 0 0 #000',
+  },
+  aboutContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  nameStyle: {
+    fontSize: '5em',
+    marginBottom: '30px',
+  },
+  imgStyle: {
+    borderRadius: '5px',
+  },
+  introTextContainer: {
+    margin: 10,
+    flexDirection: 'column',
+    whiteSpace: 'pre-wrap',
+    textAlign: 'left',
+    fontSize: '1.2em',
+    fontWeight: 500,
+  },
+  introImageContainer: {
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
+};
+
+function Portfolio() {
+  const [homeData, setHomeData] = useState(null);
+  const [aboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    // Fetch home data
+    fetch(endpoints.home, { method: 'GET' })
+      .then((res) => res.json())
+      .then((res) => setHomeData(res))
+      .catch((err) => console.error(err));
+
+    // Fetch about data
+    fetch(endpoints.about, { method: 'GET' })
+      .then((res) => res.json())
+      .then((res) => setAboutData(res))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const parseIntro = (text) => (
+    <ReactMarkdown children={text} />
+  );
+
+  return (
+    <div style={styles.portfolioContainer}>
+      {/* Home Section */}
+      <div style={styles.homeContainer}>
+        {homeData ? (
+          < >
+            <h3 style={styles.nameStyle}>{homeData?.name}</h3>
+            <img width={250} height={250} style={styles.imgStyle} src={aboutData?.imageSource} alt="profile" />
+            <Social />
+          </>
+        ) : <FallbackSpinner />}
+      </div>
+
+      {/* About Section */}
+      <div style={styles.aboutContainer}>
+        {aboutData ? (
+          <Slide direction='down'>
+            <Container>
+              <Row>
+                {parseIntro(aboutData.about)}
+              </Row>
+            </Container>
+          </Slide>
+        ) : <FallbackSpinner />}
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
