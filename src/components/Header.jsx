4d6250e34bc09d55cab3c99fa8c12beb8fc9f0f0
neilrathi17 <neilrathi17@gmail.com>
2024-10-19 13:06:30 +0530

    fixed header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 
 function Header(props) {
   const { title } = props;
-  return <h5 style={{margintop:'1px'}} className="header">{title}</h5>;
+  return <h5 style={{margintop:'1px', padding:'10px'}} className="header">{title}</h5>;
 }
 
 Header.propTypes = {
